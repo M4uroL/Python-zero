8e787bd96c6ec222be61ff632fd41c631cab6238
Mauro de Lima <168693244+M4uroL@users.noreply.github.com>
2025-10-13 21:48:55 +0100

    Add files via upload

---
+++ Operadores-Comparacao.py
@@ -0,0 +1,16 @@
+# Operadores Aritméticos
+print("____________________________")
+print("Operadores De Comparacao")
+print("____________________________")
+a = 10
+b = 5
+print("a = 10 , b = 5")
+print("____________________________")
+print("Igual: ", a == b)
+print("Diferente de ", a != b)
+print("Maior que ", a > b)
+print("Menor que ",  a < b)
+print("Maior igual que ", a >= b)
+print("Menor igual que ", a <= b)
+
+print("____________________________")
---
+++ OperadoresAritmetricos.py
@@ -0,0 +1,16 @@
+# Operadores Aritméticos
+print("____________________________")
+print("Operadores Aritmétricos")
+print("____________________________")
+a = 10
+b = 5
+print("a = 10 , b = 5")
+print("____________________________")
+print("A soma: ", a + b)
+print("A subtração ", a - b)
+print("A multiplicação ", a * b)
+print("A divisão Real ",  a / b)
+print("O modulo ", a % b)
+print("Potenciação ", a ** b)
+print("A divsão Inteira ",a // b)
+print("____________________________")
